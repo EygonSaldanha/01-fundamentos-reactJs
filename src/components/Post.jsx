8af6dfdc7218847eeccd0536621406c61cb93b9a
Eygon Saldanha <eygon11@gmail.com>
2023-02-06 16:28:28 -0300

    criacao do campo de comentario do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,13 +13,19 @@ export function Post(){
                     </div>
                 </div>
 
-                <time title='11 de maio as 08:33' dateTime='2023-02-03 18:05:00'>Publicado a 1h</time>
+                <time 
+                title='11 de maio as 08:33' 
+                dateTime='2023-02-03 18:05:00'
+                >
+                    Publicado a 1h
+                </time>
                 
             </header>
 
             <div className={styles.content}>    
             <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. 
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto 
+                que fiz no NLW Return, evento da Rocketseat. 
                 O nome do projeto é DoctorCare 🚀</p>
             <p>👉 <a href=''>jane.design/doctorcare</a></p>
             <p>
@@ -28,6 +34,16 @@ export function Post(){
                 <a href=''>#rocketseat</a>
             </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder='Deixe seu comentario'
+                />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
