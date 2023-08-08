42ee640b69733fff39034b7fc2a4bb607b0b28a7
eygon.saldanha <eygon.saldanha@dataprev.gov.br>
2023-08-07 21:52:54 -0300

    Correto msm

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,37 @@ const posts = [
   {
     id: 1,
     author: {
-      avatarUrl: "https://github.com/EygonSaldanha.png",
-      name: "Eygon",
-      role: "CTO @ RocketSeat",
+      avatarUrl: 'https://github.com/EygonSaldanha.png',
+      name: 'Eygon',
+      role: 'CTO @ RocketSeat',
     },
     content: [
-      { type: "paragraph", content: "Hello" },
+      { type: 'paragraph', content: 'Hello' },
       {
-        type: "paragraph",
+        type: 'paragraph',
         content:
-          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
       },
     ],
-    publishedAt: new Date("2022-05-03 20:00:00"),
+    publishedAt: new Date('2022-05-03 20:00:00'),
   },
   {
     id: 2,
     author: {
-      avatarUrl: "https://github.com/EygonSaldanha.png",
-      name: "Teste",
-      role: "Teste de Cargo",
+      avatarUrl: 'https://github.com/EygonSaldanha.png',
+      name: 'Teste',
+      role: 'Teste de Cargo',
     },
     content: [
-      { type: "paragraph", content: "Olá" },
+      { type: 'paragraph', content: 'Olá' },
       {
-        type: "paragraph",
+        type: 'paragraph',
         content:
-          "subir não mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+          'subir não mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
       },
+      {type: 'link', content: 'isso é um teste Galera'},
     ],
-    publishedAT: new Date("2022-05-13 20:00:00"),
+    publishedAt: new Date('2022-05-13 20:00:00'),
   },
 ];
 
